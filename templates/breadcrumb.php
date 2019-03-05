56a9fb66b22f7ff5b486ee8f2bb01fc5c724d5ae
Riccardo D'Angelo <lostcore@gmail.com>
2019-03-05 10:48:14 +0100

    Missing wrapper added

--- templates/breadcrumb.php
+++ templates/breadcrumb.php
@@ -1,8 +1,8 @@
 <?php
 if($is_woocommerce && function_exists("woocommerce_breadcrumb")){
 	woocommerce_breadcrumb([
-		'wrap_before'   => '<div class="breadcrumb-trail breadcrumbs" itemprop="breadcrumb">',
-		'wrap_after'   => '</div>',
+		'wrap_before'   => '<div class="breadcrumbs__wrapper"><div class="breadcrumb-trail breadcrumbs" itemprop="breadcrumb">',
+		'wrap_after'   => '</div></div>',
 		'delimiter'  => '<span class="sep">&nbsp;&#47;&nbsp;</span>'
 	]);
 }else{
